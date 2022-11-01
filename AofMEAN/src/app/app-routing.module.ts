5a976ecaf22f01e3a8f71f1d48ada822db78fcaa
WeerapatMatee <56258343+Account-Weerapat@users.noreply.github.com>
2022-11-01 22:11:47 +0700

    set routing

--- AofMEAN/src/app/app-routing.module.ts
+++ AofMEAN/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './components/add/add.component';
+import { UpdateComponent } from './components/update/update.component'; 
+const routes: Routes = [
+  {path:'',pathMatch:'full',redirectTo:'add'},
+  {path:'update',component:UpdateComponent}
 
-const routes: Routes = [];
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
